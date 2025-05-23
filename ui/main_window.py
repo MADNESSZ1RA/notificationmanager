ffd5bb84dc46c149fb084bded29fbe160353f787
z1ra <apostolip@yandex.ru>
2025-05-23 14:34:10 +0300

    feat: Добавлена возможность сворачивать приложение в Windows tray

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,9 +1,67 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import (
+    QMainWindow, QSystemTrayIcon, QMenu
+)
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtCore import Qt, QEvent, QTimer
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setMinimumSize(550, 250)
-        self.setMaximumSize(750,250)
-        self.setWindowTitle('Мастер уведомлений')
+        self.setMaximumSize(750, 250)
+        self.setWindowTitle("Мастер уведомлений")
+
+        icon = QIcon("ui/img/favicon.ico")
+
+        self.tray = QSystemTrayIcon(icon, self)
+        self.tray.setToolTip("Мастер уведомлений")
+
+        menu = QMenu()
+        action_show = QAction("Открыть", self)
+        action_quit = QAction("Выйти", self)
+        menu.addAction(action_show)
+        menu.addSeparator()
+        menu.addAction(action_quit)
+
+        action_show.triggered.connect(self.restore_from_tray)
+        action_quit.triggered.connect(self.quit_app)
+
+        self.tray.setContextMenu(menu)
+        self.tray.activated.connect(self.icon_activated) 
+        self.tray.show()
+
+    def icon_activated(self, reason: QSystemTrayIcon.ActivationReason):
+        if reason in (QSystemTrayIcon.Trigger, QSystemTrayIcon.DoubleClick):
+            self.restore_from_tray()
+
+    def restore_from_tray(self):
+        self.showNormal()
+        self.raise_()         
+        self.activateWindow() 
+
+    def quit_app(self):
+        self.tray.hide()
+        self.close()          
+
+    def closeEvent(self, event):
+        if self.isVisible():
+            event.ignore()
+            self.hide()
+            self.tray.showMessage(
+                "Мастер уведомлений",
+                "Приложение свернуто в трей.\n"
+                "Двойной клик по иконке — вернуть окно.",
+                QSystemTrayIcon.Information,
+                3000, 
+            )
+        else:
+            super().closeEvent(event)
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:  
+            if self.windowState() & Qt.WindowMinimized:
+                QTimer.singleShot(0, self.hide)      
+        super().changeEvent(event)
+
--- ui/pages/ui_index.py
+++ ui/pages/ui_index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'indexaEAuJL.ui'
+## Form generated from reading UI file 'indexFjnKlf.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.3
 ##
@@ -33,13 +33,13 @@ class Ui_Index(object):
 
         self.timeDateInput = QDateEdit(Form)
         self.timeDateInput.setObjectName(u"timeDateInput")
-        self.timeDateInput.setDateTime(QDateTime(QDate(2025, 5, 22), QTime(0, 0, 0)))
+        self.timeDateInput.setDateTime(QDateTime(QDate(2025, 5, 21), QTime(21, 0, 0)))
 
         self.gridLayout.addWidget(self.timeDateInput, 1, 0, 1, 1)
 
         self.timeTimeInput = QTimeEdit(Form)
         self.timeTimeInput.setObjectName(u"timeTimeInput")
-        self.timeTimeInput.setDateTime(QDateTime(QDate(2000, 1, 1), QTime(9, 0, 0)))
+        self.timeTimeInput.setDateTime(QDateTime(QDate(2000, 1, 1), QTime(6, 0, 0)))
 
         self.gridLayout.addWidget(self.timeTimeInput, 1, 1, 1, 1)
 
@@ -105,7 +105,7 @@ class Ui_Index(object):
         self.timeTitleInput.setPlaceholderText(QCoreApplication.translate("Form", u"\u0417\u0430\u0433\u043e\u043b\u043e\u0432\u043e\u043a", None))
         self.timeDescriptionInput.setPlaceholderText(QCoreApplication.translate("Form", u"\u0421\u043e\u0434\u0435\u0440\u0436\u0430\u043d\u0438\u0435", None))
         self.timeAddButton.setText(QCoreApplication.translate("Form", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0443\u0432\u0435\u0434\u043e\u043c\u043b\u0435\u043d\u0438\u0435", None))
-        self.label_2.setText(QCoreApplication.translate("Form", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c \u0443\u0432\u0435\u0434\u043e\u043c\u043b\u0435\u043d\u0438\u0435 \u0441 \u0437\u0430\u0434\u0435\u0440\u0436\u043a\u043e\u0439 \u0432 \u0441\u0435\u043a\u0443\u043d\u0434\u0430\u0445", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c \u0443\u0432\u0435\u0434\u043e\u043c\u043b\u0435\u043d\u0438\u0435 \u0441 \u0437\u0430\u0434\u0435\u0440\u0436\u043a\u043e\u0439 \u0432 \u043c\u0438\u043d\u0443\u0442\u0430\u0445", None))
         self.waitTitleInput.setPlaceholderText(QCoreApplication.translate("Form", u"\u0417\u0430\u0433\u043e\u043b\u043e\u0432\u043e\u043a", None))
         self.waitDescriptionInput.setPlaceholderText(QCoreApplication.translate("Form", u"\u0421\u043e\u0434\u0435\u0440\u0436\u0430\u043d\u0438\u0435", None))
         self.waitAddButton.setText(QCoreApplication.translate("Form", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0443\u0432\u0435\u0434\u043e\u043c\u043b\u0435\u043d\u0438\u0435", None))
